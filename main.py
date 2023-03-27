5ca39eaaffe80bc7732a284ba8036fdfbb14dad1
gorhgo3 <msin981@gmail.com>
2023-03-28 12:30:38 +1300

    templates and static folder removing extra HTML files and updating CSS and static roots on HTML files for new added CSS/style file

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-
-@app.route('/login', methods=['POST'])
-def meke():
-    # rqst = request.form['chur-form']
-    # rqst1 = request.form['churr-form']
-    # return f'<h1> {rqst}, {rqst1} </h1>'
-    return render_template('index2.html')
+    return render_template('home.html')
 
 
 if __name__ == '__main__':
